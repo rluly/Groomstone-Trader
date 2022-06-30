a9f3b2e248b45959e813b24c2e60ca6e430566fd
rluly <ryanluly1@gmail.com>
2022-06-30 13:47:12 -0700

    Clean up analysis code and make it iterable

--- analysis.py
+++ analysis.py
@@ -1,41 +1,64 @@
-#Placeholder
+# Imports
 import csv
 import json
 
+# Global Vars
+tickers = []
 close = []
 volume = []
-cap = 0
-ev = 0
 ebitda_list = []
 cash = []
 longdebt = []
 shortdebt = []
 
-with open('./data/IBM_daily.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        close.append(row['close'])
-        volume.append(row['volume'])
+def parse_Daily(tick):
+    path = './data/' + tick + '/' + tick + '_daily.csv'
+    with open(path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            close.append(row['close'])
+            volume.append(row['volume'])
 
-cap = round(float(close[0])) * int(volume[0])
-# print(cap)
+def parse_Income(tick):
+    path = './data/' + tick + '/' + tick + '_income.json'
+    f = open(path)
+    data = json.load(f)
+    for i in data['quarterlyReports']:
+        ebitda_list.append(i['ebitda'])
 
+def parse_Balance(tick):
+    path = './data/' + tick + '/' + tick + '_balance.json'
+    f = open(path)
+    data = json.load(f)
+    for i in data['quarterlyReports']:
+        cash.append(i['cashAndCashEquivalentsAtCarryingValue'])
+        longdebt.append(i['currentLongTermDebt'])
+        shortdebt.append(i['shortLongTermDebtTotal'])
 
-f = open('./data/IBM_income.json')
-data = json.load(f)
+def calc_EV(tick):
+    cap = round(float(close[0])) * int(volume[0])
+    ev = cap + int(longdebt[0]) + int(shortdebt[0]) - int(cash[0])
+    return ev
 
-for i in data['quarterlyReports']:
-    ebitda_list.append(i['ebitda'])
+def calc_EBITDA(tick):
+    ebitda = int(ebitda_list[0])
+    return ebitda
 
+def calc_EVEBITDA(tick):
+    return calc_EV(tick)/calc_EBITDA(tick)
 
-f = open('./data/IBM_balance.json')
-data = json.load(f)
+def full_Parse(tick):
+    parse_Daily(tick)
+    parse_Income(tick)
+    parse_Balance(tick)
 
-for i in data['quarterlyReports']:
-    cash.append(i['cashAndCashEquivalentsAtCarryingValue'])
-    longdebt.append(i['currentLongTermDebt'])
-    shortdebt.append(i['shortLongTermDebtTotal'])
+def full_Analysis(tick):
+    print(calc_EVEBITDA(tick))
 
-ev = cap + int(longdebt[0]) + int(shortdebt[0]) - int(cash[0])
-ebitda = int(ebitda_list[0])
-print(ev/ebitda)
+f = open("tickers.txt","r")
+for x in f:
+    tickers.append(x.strip())
+
+for tick in tickers:
+    full_Parse(tick)
+    full_Analysis(tick)
