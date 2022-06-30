d4a031b9e2edce5aedb19382a3168abe558c7f22
rluly <ryanluly1@gmail.com>
2022-06-29 22:10:52 -0700

    Add test script for data collection

--- collection.py
+++ collection.py
@@ -1,1 +1,6 @@
-# Placeholder
+from alpha_vantage.timeseries import TimeSeries
+
+app = TimeSeries() #set up your api key as an environment variable ALPHAVANTAGE_API_KEY
+
+aapl = app.get_daily(symbol='AAPL',outputsize='compact')
+print(aapl)
