5bfe0c3b9857df4a05ac0238badea2e7225db52e
rluly <ryanluly1@gmail.com>
2022-06-30 12:30:01 -0700

    Test Alpha Vantage functions and add analysis placeholder

--- collection.py
+++ collection.py
@@ -1,6 +1,39 @@
-from alpha_vantage.timeseries import TimeSeries
+#set up your api key as an environment variable ALPHAVANTAGE_API_KEY
 
-app = TimeSeries() #set up your api key as an environment variable ALPHAVANTAGE_API_KEY
+# from alpha_vantage.timeseries import TimeSeries
+# from alpha_vantage.foreignexchange import ForeignExchange
+# from alpha_vantage.cryptocurrencies import CryptoCurrencies
+# from alpha_vantage.techindicators import TechIndicators
+# from alpha_vantage.sectorperformance import SectorPerformances
+import os
+import requests
+import pandas as pd
 
-aapl = app.get_daily(symbol='AAPL',outputsize='compact')
-print(aapl)
+# app = TimeSeries()
+# aapl = app.get_daily(symbol='AAPL',outputsize='compact')
+# print(aapl)
+
+api_key = os.getenv('ALPHAVANTAGE_API_KEY')
+
+#EBITA (as is)
+base_url = 'https://www.alphavantage.co/query?'
+params = {'function': 'INCOME_STATEMENT',
+		 'symbol': 'IBM', 
+		 'apikey': api_key}
+
+response = requests.get(base_url,params=params)
+with open('./data/IBM_income.json','wb') as file:
+    file.write(response.content)
+
+#EV (long debt, short debt, and cash)
+base_url = 'https://www.alphavantage.co/query?'
+params = {'function': 'BALANCE_SHEET',
+		 'symbol': 'IBM', 
+		 'apikey': api_key}
+
+response = requests.get(base_url,params=params)
+with open('./data/IBM_balance.json','wb') as file:
+    file.write(response.content)
+
+# df = pd.read_csv('./data/IBM.csv')
+# df.set_index('timestamp',inplace=True)
